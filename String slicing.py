ee1a3707459db6a4023a29fd8942d40711dae8e9
vansh029 <75217824+vansh029@users.noreply.github.com>
2023-02-23 23:50:18 +0530

    Add files via upload

---
+++ String slicing.py
@@ -0,0 +1,16 @@
+name="vansh bansal"
+print(len(name))
+print(name[0:6])
+
+fruit="mango"
+print("mango is a ",len(fruit)," letter word")
+mangolen=len(fruit)
+print(fruit[0:4])
+print(fruit[1:4])
+print(fruit[:5])
+print(fruit[0: -3])
+print(fruit[0:len(fruit)-3])
+print(fruit[0:len(fruit)-6])
+
+nm="harry"
+print(nm[-4:-2])
---
+++ strings.py
@@ -0,0 +1,33 @@
+#normal printing
+a="Apple";
+print(a)
+
+#without using escape sequal characters
+b='Hello "this" is an apple'
+print(b)
+
+#how to write a paragraph(Use \n)
+c="Hello i am a \ngood \nboiiiii "
+print(c)
+
+#how to write a paragraph(Use single couts)
+d='''Hello i am a
+good 
+boiiiii'''
+print(d)
+
+# indexing in string
+e='vansh'
+print(e[0])
+print(e[1])
+print(e[2])
+print(e[3])
+print(e[4])
+#print(e[5]) throws an apple
+
+#indexing in string using a for loop
+for character in e:
+    print(character)
+    
+for charactar in b:
+    print(charactar)
